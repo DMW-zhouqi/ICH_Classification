15b05c3dbe58c812721b37ad4526f5ea2f73b97e
DMW-zhouqi <61447046+DMW-zhouqi@users.noreply.github.com>
2020-12-01 11:54:16 +0800

    Add files via upload

---
+++ main_ResNet18.py
@@ -0,0 +1,92 @@
+# Import the relevant packages
+import torch
+from torch import optim, nn
+import torchvision
+from torch.utils.data import DataLoader
+from MakeDataset import makedataset
+from torchvision.models import resnet18
+from utils import Flatten
+
+# Set hyper-parameters
+batchsz = 4
+lr = 1e-3
+epochs = 10
+
+# Specify CUDA acceleration and set random seeds
+device = torch.device('cuda')
+torch.manual_seed(1234)
+
+# load train/val/test datasets
+train_db = makedataset('mydata', 224, mode='train')
+val_db = makedataset('mydata', 224, mode='val')
+test_db = makedataset('mydata', 224, mode='test')
+train_loader = DataLoader(train_db, batch_size=batchsz, shuffle=True)
+val_loader = DataLoader(val_db, batch_size=batchsz)
+test_loader = DataLoader(test_db, batch_size=batchsz)
+
+# view numbers of datasets
+print('num_train:', len(train_loader.dataset))
+print('num_val:', len(val_loader.dataset))
+print('num_test:', len(test_loader.dataset))
+
+# Define validation functions
+def evalute(model, loader):
+    model.eval()
+
+    correct = 0
+    total = len(loader.dataset)
+
+    for x, y in loader:
+        x, y = x.to(device), y.to(device)
+        with torch.no_grad():
+            logits = model(x)
+            pred = logits.argmax(dim=1)
+        # Calculate the correct number of predictions in the validation set
+        correct += torch.eq(pred, y).sum().float().item()
+
+    return correct / total # Return accuracy
+
+# Define main function training
+def main():
+    # Load the pre-training model
+    trained_model = resnet18(pretrained=True)
+    # Use the transfer learning replacement structure
+    model = nn.Sequential(*list(trained_model.children())[:-1],  # [b, 512, 1, 1]
+                          Flatten(),  # [b, 512, 1, 1] => [b, 512]
+                          nn.Linear(512, 6)
+                          ).to(device)
+    # Set optimizer and loss function
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    criteon = nn.CrossEntropyLoss()
+
+    # Start training
+    best_acc, best_epoch = 0, 0
+    # The number of iterations
+    for epoch in range(epochs):
+        for step, (x, y) in enumerate(train_loader):
+            # x: [b, 3, 224, 224], y: [b]
+            x, y = x.to(device), y.to(device)
+            model.train()
+            logits = model(x)
+            loss = criteon(logits, y)
+
+            # Back propagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # Visual loss value
+        if epoch % 1 == 0:
+            val_acc = evalute(model, val_loader)
+            print('Epoch:', epoch, '/', epochs-1, 'acc_val:', val_acc, 'loss:', loss)
+            if val_acc > best_acc:
+                best_epoch = epoch
+                best_acc = val_acc
+                print(best_acc)
+                # Save the model weight parameter values
+                torch.save(model.state_dict(), 'mydata/weights_resnet18.mdl')
+    # Print the best training result
+    print('best acc:', best_acc, 'best epoch:', best_epoch)
+
+if __name__ == '__main__':
+    main()
---
+++ test_DenseNet121.py
@@ -0,0 +1,96 @@
+# Import the relevant packages
+import torch
+from torch import optim, nn
+import torchvision
+from torch.utils.data import DataLoader
+from MakeDataset import makedataset
+from torchvision.models import densenet121
+from utils import Flatten
+
+# Set the batch of the test set
+batchsz = 4
+
+# Specify CUDA acceleration and set random seeds
+device = torch.device('cuda')
+torch.manual_seed(1234)
+
+# load test datasets
+test_db = makedataset('mydata', 224, mode='test')
+test_loader = DataLoader(test_db, batch_size=batchsz)
+print('num_test:', len(test_loader.dataset)) # # view numbers of datasets
+
+# Define test functions
+def test(model, loader):
+    model.eval()
+
+    correct = 0
+    total = len(loader.dataset)
+
+    # Set the initial values for each subtype
+    num_EDH, num_IVH, num_normal, num_CPH, num_SAH, num_SDH = 0, 0, 0, 0, 0, 0
+    # Set the model to predict the initial values for each subtype
+    num_EDH_T, num_IVH_T, num_normal_T, num_CPH_T, num_SAH_T, num_SDH_T = 0, 0, 0, 0, 0, 0
+
+    for x, y in loader:
+        x, y = x.to(device), y.to(device)
+        for label in y:
+            if label == 0:
+                num_EDH += 1
+            if label == 1:
+                num_IVH += 1
+            if label == 2:
+                num_normal += 1
+            if label == 3:
+                num_CPH += 1
+            if label == 4:
+                num_SAH += 1
+            if label == 5:
+                num_SDH += 1
+        with torch.no_grad():
+            logits = model(x)
+            pred = logits.argmax(dim=1)
+            # The update model predicts the number of positives
+            for i in range(len(pred)):
+                for j in range(i, len(y)):
+                    if pred[i] == y[j] and y[j] == 0:
+                        num_EDH_T += 1
+                    if pred[i] == y[j] and y[j] == 1:
+                        num_IVH_T += 1
+                    if pred[i] == y[j] and y[j] == 2:
+                        num_normal_T += 1
+                    if pred[i] == y[j] and y[j] == 3:
+                        num_CPH_T += 1
+                    if pred[i] == y[j] and y[j] == 4:
+                        num_SAH_T += 1
+                    if pred[i] == y[j] and y[j] == 5:
+                        num_SDH_T += 1
+                    break
+        # Calculate the total number of correct Numbers
+        correct += torch.eq(pred, y).sum().float().item()
+
+    # visualization
+    print('num_EDH:', num_EDH, 'num_EDH_T:', num_EDH_T)
+    print('num_IVH:', num_IVH, 'num_IVH_T:', num_IVH_T)
+    print('num_normal:', num_normal, 'num_normal_T:', num_normal_T)
+    print('num_CPH:', num_CPH, 'num_CPH_T:', num_CPH_T)
+    print('num_SAH:', num_SAH, 'num_SAH_T:', num_SAH_T)
+    print('num_SDH:', num_SDH, 'num_SDH_T:', num_SDH_T)
+
+    return correct / total # Return accuracy
+
+# Define main function test
+def main():
+    # Load the pre-training model
+    trained_model = densenet121(pretrained=True)
+    # Use the transfer learning replacement structure
+    model = nn.Sequential(*list(trained_model.children())[:-1],  # [b, 512, 1, 1]
+                          Flatten(),  # [b, 512, 1, 1] => [b, 512]
+                          nn.Linear(512, 6)
+                          ).to(device)
+    # Start test
+    test_acc = test(model, test_loader)
+    # print final accuracy
+    print('test acc:', test_acc)
+
+if __name__ == '__main__':
+    main()
